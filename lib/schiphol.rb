99478a5215542b23dce9bef89927e75eab506e94
Paul Anderson <anderson.paul.martin@gmail.com>
2016-01-11 23:51:06 -0500

    changed rubyzip require statement

--- lib/schiphol.rb
+++ lib/schiphol.rb
@@ -13,7 +13,8 @@ module Schiphol
   # Require fileutils to move files around.
   require 'fileutils'
   # Require rubyzip to unzip packages.
-  require 'zip/zip'
+  require 'zip'
+  #require 'zip/zip' in previous versions
   # Require progressbar to track progress.
   require 'progressbar'
   
